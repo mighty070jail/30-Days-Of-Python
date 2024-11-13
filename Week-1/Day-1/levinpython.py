c1ef0394930a7e791e38a5215ebb106be460acab
levinabr <levinabrahamjacob2028@cs.sjcetpalai.ac.in>
2024-11-13 20:30:56 +0530

    levinpython.py

--- Week-1/Day-1/levinpython.py
+++ Week-1/Day-1/levinpython.py
@@ -1,5 +1,6 @@
 """
-Author - Levin Abraham Jacob
+Name - Levin Abraham Jacob
+Date - 13 -11 - 2024
 Description - Simple Python Program To Print  "Hello, World"
 """
 print("Hello, World.") #Print is used to Display Words.
