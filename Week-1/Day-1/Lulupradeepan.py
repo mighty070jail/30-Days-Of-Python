03f02eb9f48de7128225fabdc3c7264c13788739
Lulup-pradeepan <lulupradeepan@gmail.com>
2024-11-13 23:00:14 +0530

    Lulupradeepan.py

---
+++ Week-1/Day-1/Lulupradeepan.py
@@ -0,0 +1,11 @@
+'''
+Name:Lulu Pradeepan
+Date:13-11-2024
+Description:Python program to print a string
+'''
+string_1="HELLO,"
+string_2="WORLD!"
+string=string_1+''+string_2
+print(string)
+string_3="Learning python is exciting!"
+print(string_3)
