f44b21a84b9719f5d99edfb4d67b02d14046e330
Leslythomasmathew <Leslymathew1306@gmail.com>
2024-11-13 21:27:58 +0530

    Create leslythomas.py

---
+++ Week-1/Day-1/leslythomas.py
@@ -0,0 +1,11 @@
+'''
+Name: Lesly Mathew
+Date:13-11-2024
+Description: python program to print a string
+'''
+str_1="Hello,"
+str_2="World!"
+str=str_1+' '+str_2
+print(str)
+str_3="I am ready to code!"
+print(str_3)
