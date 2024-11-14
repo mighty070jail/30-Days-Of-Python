38354659c190921c53221e357505cdad74764fed
Leslythomasmathew <Leslymathew1306@gmail.com>
2024-11-14 20:56:11 +0530

    Create Lesly2.py

---
+++ Week-1/Day-2/Lesly2.py
@@ -0,0 +1,9 @@
+'''
+Name: Lesly Mathew
+Date: 14-11-2024
+Description:To Understand variables and how to store and use data.
+'''
+name="Lesly Mathew"
+age=18
+color="blue"
+print("My name is",name,",I am",age,"years old and, I love ",color,".")
