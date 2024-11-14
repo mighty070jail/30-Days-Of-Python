649f7ae6b9bd0f1b505555eaca66727f789b81e6
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2024-11-14 17:46:48 +0530

    Upload joshilsabraham.py

---
+++ Week-1/Day-1/joshilsabraham.py
@@ -0,0 +1,15 @@
+'''
+Author: Joshil S Abraham
+Date: 14 / 11 / 2024
+Description: Python program to use print() function
+'''
+string_1="Hello, "
+string_2="World!"
+print(string_1+string_2)
+string_3="Learning Python is exciting!"
+print(string_3)
+'''
+OUTPUT:
+Hello, World!
+Learning Python is exciting!
+'''
