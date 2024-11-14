8a41c8b2117ef51f6e97019764dfbc9c569a67be
levinabr <levinabrahamjacob2028@cs.sjcetpalai.ac.in>
2024-11-14 20:22:06 +0530

    Add files via upload

---
+++ Week-1/Day-2/D2LevinPython.py
@@ -0,0 +1,12 @@
+"""
+Name  - Levin Abraham Jacob
+Date - 13-11-2024
+Description : Understand variables and how to store and use data.
+
+"""
+name = "Levin Abraham Jacob" #variable to store name.
+age = 18 #variable to store age.
+fav_color = "Black" #variable to store color.
+
+print(f"\nHello my name is {name}, I am {age} Years old and My favorite Color is {fav_color} .")
+#Above Line Prints variables within a string...
