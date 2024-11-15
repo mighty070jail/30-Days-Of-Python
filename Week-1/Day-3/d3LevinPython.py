0e8f6c838f10167c65ad0620d6b1bcb4cde6f275
Levin Abraham Jacob <levinabrahamjacob2028@cs.sjcetpalai.ac.in>
2024-11-16 00:57:39 +0530

    Add files via upload

---
+++ Week-1/Day-3/d3LevinPython.py
@@ -0,0 +1,12 @@
+"""
+Author = Levin Abraham Jacob
+Date = 15-11-2024
+Aim = Basic Arithmetic Operations
+"""
+num1 = 50#stores value of number 1
+num2 = 20#stores value of number 2
+print(f"First Number Is {num1},Second Number is {num2}")#displays both numbers
+print(f"The sum of Numbers is {num1+num2}")#displays sum of numbers
+print(f"The difference of Numbers is {num1-num2} or{num2-num1}")#displays subtraction of both numbers
+print(f"The product of Numbers is {num1*num2}")#display product of both numbers
+print(f"The quotient of Numbers is {num1/num2}")#display the quotient when number 1 is divided by number 2
