5aed59697df237ab916fdbbbedd7517f349c22b5
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2024-11-15 12:02:18 +0530

    Create joshilsabraham.py

---
+++ Week-1/Day-2/joshilsabraham.py
@@ -0,0 +1,11 @@
+"""
+Name  - Joshil S Abraham
+Date - 15-11-2024
+Description : Understand variables and how to store and use data.
+"""
+name = "Joshil S Abraham" #variable to store name.
+age = 18 #variable to store age.
+fav_color = "Blue" #variable to store color.
+
+print(f"\nHello my name is {name}, I am {age} Years old and My favorite Color is {fav_color} .")
+#Above Line Prints variables within a string..
