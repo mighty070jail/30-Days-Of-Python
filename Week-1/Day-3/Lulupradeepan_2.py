f318c8750ceaea147d48f88ae3f77a26f9d5b3df
Lulup-pradeepan <lulupradeepan@gmail.com>
2024-11-16 16:01:19 +0530

    Lulupradeepan_2.py

---
+++ Week-1/Day-3/Lulupradeepan_2.py
@@ -0,0 +1,15 @@
+'''
+Name:Lulu Pradeepan
+Date:15-11-2024
+Description:Basic arithmetic operators in python
+'''
+num_1=25
+num_2=20
+sum=num_1+num_2
+print(sum)
+difference=num_1-num_2
+print(difference)
+product=num_1*num_2
+print(product)
+quotient=num_1/num_2
+print(quotient)
