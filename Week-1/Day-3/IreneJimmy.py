5291ba7780d8193ae003fee1b993fec3ee946e43
irenejimmy <irenejimmy2028@cs.sjcetpalai.ac.in>
2024-11-17 17:29:32 +0530

    Add files via upload

---
+++ Week-1/Day-3/IreneJimmy.py
@@ -0,0 +1,15 @@
+'''
+Author:Irene
+Date:16-11-24
+Description:Arithmetic operators in python
+'''
+num1=5
+num2=10
+sum=num1+num2
+print(sum)
+diff=num2-num1
+print(diff)
+pro=num1*num2
+print(pro)
+quo=num2/num1
+print(quo)
