e2ae7279f9f4deca6b8381d05a49589fd17d6783
Leslythomasmathew <Leslymathew1306@gmail.com>
2024-11-18 20:59:52 +0530

    Create Lesly3.py

---
+++ Week-1/Day-3/Lesly3.py
@@ -0,0 +1,15 @@
+'''
+Name:Lesly Mathew
+Date:18-11-2024
+Description: program to calculate the sum,difference,product and quotient of two numbers
+'''
+num_1=int(input("Enter any number: "))
+num_2=int(input("Enter any number: "))
+sum=num_1+num_2
+print("The Sum of the given two numbers is: ",sum,)
+difference=num_1-num_2
+print("The Difference of the given two numbers is: ",difference )
+product=num_1*num_2
+print("The Product of the given two numbers is:",product)
+quotient=num_1/num_2
+print("The Quotient of the given two numbers is:",quotient)
