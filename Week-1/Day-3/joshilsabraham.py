0a336f72a3300cca681c8cc5b9daf6942a42b127
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2024-11-20 15:56:10 +0530

    Create joshilsabraham.py

---
+++ Week-1/Day-3/joshilsabraham.py
@@ -0,0 +1,21 @@
+"""
+Author = Joshil S Abraham
+Date = 20-11-2024
+Description = Basic Arithmetic Operations in Python
+"""
+num1 = 10 #stores value of number 1
+num2 = 5 #stores value of number 2
+print(f"First Number is {num1}, Second Number is {num2}") #Displays Both Numbers
+print(f"The Sum of numbers is {num1+num2}") #Displays Sum of Numbers
+print(f"The Product of numbers is {num1*num2}") #Displays Product of Numbers
+print(f"The Difference of numbers is {num1-num2}") #Displays Difference of Both Numbers
+print(f"The Quotient of Numbers is {num1/num2}") #Display the Quotient when number 1 is divided by number 2
+
+'''
+OUTPUT:
+First Number is 10, Second Number is 5
+The Sum of numbers is 15
+The Product of numbers is 50
+The Difference of numbers is 5
+The Quotient of Numbers is 2.0
+'''
