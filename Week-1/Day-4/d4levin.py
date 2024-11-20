dc85bc0fc3112c70422a177dda746a564c32652e
Levin Abraham Jacob <levinabrahamjacob2028@cs.sjcetpalai.ac.in>
2024-11-20 20:49:23 +0530

    Add files via upload

---
+++ Week-1/Day-4/d4levin.py
@@ -0,0 +1,46 @@
+"""
+Author - Levin Abraham Jacob
+date = 20-11-2024
+Description:
+Practice basic Python operations like creating variables, manipulating their values,
+ swapping variables and combining strings.
+"""
+#Exercise 1: Basic Variable Creation and Printing
+x = 10
+y =20
+print(x)#display value of x
+print(y)#display value of y
+"""
+Output : 
+10
+20
+"""
+#Exercise 2: Swapping Variables
+num1 = 5
+num2 = 10
+num1 = num1+num2#sums num1 and num2 new value + 15
+num2 = num1-num2# subtract 10 from 15 = 5
+num1 = num1-num2# subtract 5 from 15 = 10
+print(num1)
+print(num2)
+"""
+Output :
+10 
+5
+"""
+#Exercise 3: Combining Strings and Variables
+name = "Levin Abraham Jacob"
+greeting = "Hello , "
+print(greeting+name)
+"""
+Output :
+Hello , Levin Abraham Jacob
+"""
+#Exercise 4: Updating Variables
+counter = 0
+counter +=5*3 #incriminates counter 3 times with 5
+print(counter)
+"""
+Output :
+15
+"""
