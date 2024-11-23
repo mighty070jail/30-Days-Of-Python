be243033da37a6c0290378814e0d37375fc409e5
irenejimmy <irenejimmy2028@cs.sjcetpalai.ac.in>
2024-11-23 09:07:18 +0530

    Add files via upload

---
+++ Week-1/Day-4/irenejimmy4.py
@@ -0,0 +1,26 @@
+#TASK 1 Basic Variable Creation and Printing
+x=10
+y=20
+print(x)
+print(y)
+
+#TASK 2 Swapping Variables
+num1=5
+num2=10
+num3=num1
+num1=num2
+num2=num3
+print("Num1=",num1)
+print("Num2=",num2)
+
+#TASK3 Combining Strings and Variables
+name="Irene"
+greetings="Hello,"
+message=greetings+" "+name
+print(message)
+
+#TASK4 Combining Strings and Variables
+counter=0
+while counter<15:
+    counter=counter+5
+    print(counter)
