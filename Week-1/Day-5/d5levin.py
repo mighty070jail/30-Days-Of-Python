c14d08277e15a3057d2ca92a31d5393dbac739d7
Levin Abraham Jacob <levinabrahamjacob2028@cs.sjcetpalai.ac.in>
2024-11-24 11:14:40 +0530

    Add files via upload

---
+++ Week-1/Day-5/d5levin.py
@@ -0,0 +1,19 @@
+"""
+Author = Levin Abraham Jacob
+date = 24-11-2024
+description =
+Task 1 = Calculate number of seconds in 1 year (using variables)
+Task 2 = Calculate number of seconds in 4 years (using variables)
+
+"""
+
+
+
+
+days_per_year = 365
+hours_per_day = 24
+minutes_per_hour = 60
+secs_per_min = 60
+print( f"The Number of Seconds in 1 Year ={secs_per_min*hours_per_day*days_per_year*minutes_per_hour}")
+
+print(f"The Number of Seconds in 4 Years ={secs_per_min*hours_per_day*days_per_year*minutes_per_hour*4}",)
