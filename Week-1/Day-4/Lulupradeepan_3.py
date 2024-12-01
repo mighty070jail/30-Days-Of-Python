0be66cab857209843dad4a17e9a4c6955d23009e
Lulup-pradeepan <lulupradeepan@gmail.com>
2024-12-01 16:27:33 +0530

    Lulupradeepan_3.py

---
+++ Week-1/Day-4/Lulupradeepan_3.py
@@ -0,0 +1,31 @@
+'''
+name:Lulu Pradeepan
+Date:26-11-2024
+Description:python operators
+'''
+#excercise 1:Basic variable creation and printing
+x=10
+y=20
+print(x)
+print(y)
+#excercise 2:Swapping Variables
+num1=5
+num2=10
+num3=num1
+num_1=num2
+num_2=num3
+print("num_1=",num_1)
+print("num_2=",num_2)
+#excercise 3:Combining strings and variables
+name="Lulu pradeepan"
+greetings="Hello,"
+message=greetings+" "+name
+print(message)
+#excercise 4:Updating variables
+counter=0
+counter+=5*3
+print(counter)
+
+
+
+
