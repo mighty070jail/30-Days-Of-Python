8f401ebd9408acf289e2ef3684566a90d2dc94e4
Lulup-pradeepan <lulupradeepan@gmail.com>
2024-12-01 16:28:57 +0530

    Lulupradeepan_4.py

---
+++ Week-1/Day-5/Lulupradeepan_4.py
@@ -0,0 +1,15 @@
+'''
+name:Lulu Pradeepan
+Date:26-11-2024
+Description:Task_1:Calculate the number of seconds in 1 years
+            Task_2:Calculate the number of seconds in 4 years
+'''
+num_years=4
+days_per_year=365
+hours_per_day=24
+mins_per_hour=60
+secs_per_min=60
+num_of_seconds=days_per_year*hours_per_day*mins_per_hour*secs_per_min
+print(num_of_seconds)
+num_of_seconds_in_4years=num_years*days_per_year*hours_per_day*mins_per_hour*secs_per_min
+print(num_of_seconds_in_4years)
